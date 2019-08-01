e2340669bdaee8465e7a3dccb8bbc5486b81691d
marko1996dj <marko96djuric@gmail.com>
2019-08-01 17:35:44 +0200

    axios implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      api_key: '8ff61bbd5329bd86f6c3fb9c61a115ba',
+      movie: null
+    }
+    this.request = this.request.bind(this);
+  }
+
+  request = () => {
+    axios.get(`https://api.themoviedb.org/3/movie/76341?api_key=${this.state.api_key}`).then(
+      (response) => {
+        const movie = response.data;
+        this.setState({movie: movie})
+        console.log(response.data)
+      }
+    ).catch(e => {
+      console.log(e);
+    })
+  }
+
+  componentDidMount = () => {
+    this.request()
+  }
+
+
+  render() {
+    let moviePoster;
+    if(this.state.movie) {
+      moviePoster = this.state.movie.belongs_to_collection.poster_path
+      console.log(moviePoster);
+    }
+
+
+
+    return(
+      <div className="App">
+        <h1>the-movie-db</h1>
+        <img src={moviePoster}></img>
+      </div>
+    );
+  }
 }
 
 export default App;
